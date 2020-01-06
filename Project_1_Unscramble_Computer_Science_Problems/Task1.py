273362658114e5d00bffcb47adab6eb1663f54c1
Omar Ahmed <mecharobot@yahoo.com>
2020-01-07 00:01:55 +0200

    Task1 in project1 is finished

--- Project_1_Unscramble_Computer_Science_Problems/Task1.py
+++ Project_1_Unscramble_Computer_Science_Problems/Task1.py
@@ -18,3 +18,31 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+
+
+def add_numbers(file:list, tel_numbers:set):
+    """
+    Given a file adds all available numbers into a set
+    inputs
+    param file: file containing telephone numbers
+    param tel_numbers: variable used to contain the telephone numbers
+    output
+    return: tel_numbers with file numbers extracted
+    """
+    for column in [0, 1]:
+        temp_numbers = [data[column] for data in file]
+        print(len(temp_numbers))
+        # accepts another iterable & ignor the same elements
+        tel_numbers.update(temp_numbers)
+    return tel_numbers
+
+
+if __name__ == "__main__":
+    tel_numbers = set()
+
+    tel_numbers = add_numbers(calls, tel_numbers)
+    tel_numbers = add_numbers(texts, tel_numbers)
+
+    print("There are {} different telephone numbers in the records.".format(len(tel_numbers)))
+
+
